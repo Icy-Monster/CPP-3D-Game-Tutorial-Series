1f0ead918c9f2b53023b44842e054f3a81fa2cd5
PardCode <45696173+PardCode@users.noreply.github.com>
2022-10-09 15:27:50 +0200

    Assets And Libs Tut 35
    Assets And Libs Tut 35

--- Assets/Shaders/SkyBox.hlsl
+++ Assets/Shaders/SkyBox.hlsl
@@ -35,7 +35,6 @@ struct VS_OUTPUT
 {
 	float4 position: SV_POSITION;
 	float2 texcoord: TEXCOORD0;
-	float3 normal: NORMAL0;
 };
 
 
@@ -57,7 +56,7 @@ VS_OUTPUT vsmain(VS_INPUT input)
 	output.position = mul(output.position, view);
 	output.position = mul(output.position, proj);
 	output.texcoord = input.texcoord;
-	output.normal = normalize(mul(input.normal, (float3x3)world));
+
 	return output;
 }
 
@@ -65,7 +64,6 @@ struct PS_INPUT
 {
 	float4 position: SV_POSITION;
 	float2 texcoord: TEXCOORD0;
-	float3 normal: NORMAL0;
 };
 
 Texture2D Color: register(t0);
@@ -73,34 +71,6 @@ sampler ColorSampler: register(s0);
 
 float4 psmain(PS_INPUT input) : SV_TARGET
 {
-	float4 color = Color.Sample(ColorSampler, input.texcoord);
-	float3 light_direction = normalize(float3(-1, 1, 1));
+	return Color.Sample(ColorSampler, input.texcoord);
 
-
-	//AMBIENT LIGHT
-	float ka = 8.5;
-	float3 ia = float3(0.09, 0.082, 0.082);
-	ia *= (color.rgb);
-
-	float3 ambient_light = ka * ia;
-
-	//DIFFUSE LIGHT
-	float kd = 0.7;
-	float amount_diffuse_light = max(dot(light_direction.xyz, input.normal),0.0);
-	float3 id = float3(1, 1, 1);
-	id *= (color.rgb);
-	float3 diffuse_light = kd * id * amount_diffuse_light;
-
-	//SPECULAR LIGHT
-	//float ks = 0.0;
-	//float3 is = float3(1.0, 1.0, 1.0);
-	//float3 reflected_light = reflect(m_light_direction.xyz, input.normal);
-	//float shininess = 30.0;
-	//float amount_specular_light = pow(max(0.0, dot(reflected_light, input.direction_to_camera)), shininess);
-
-	//float3 specular_light = ks * amount_specular_light * is;
-
-	float3 final_light = ambient_light + diffuse_light /*+ specular_light*/;
-
-	return float4(final_light,1.0);
 }
